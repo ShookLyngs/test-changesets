d285909636376353894bf3b57dbb4ba9869a5a8b
Shook <shooklyngs@gmail.com>
2024-05-17 10:57:30 +0800

    refactor: update ver

---
+++ .changeset/flat-chefs-wait.md
@@ -0,0 +1,5 @@
+---
+"test-changesets-action": minor
+---
+
+Change ver variable to "0.2.0"
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export const ver = '0.1.0';
+export const ver = '0.2.0';
 export const content = 'fixed commit 2';
